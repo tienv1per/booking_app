fe897a3e785c9345aaad806fb1f0d046872d84d5
tienv1per <tienv1perds@gmail.com>
2023-04-08 14:24:50 +0700

    add featuredProperties, MailList, propertyList components

---
+++ src/components/featured/FeaturedProperties.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import './FeaturedProperties.css';
+
+const FeaturedProperties = () => {
+  return (
+    <div className='fp'>
+        <div className='fpItem'>
+            <img
+                src='https://cf.bstatic.com/xdata/images/xphoto/square300/57584488.webp?k=bf724e4e9b9b75480bbe7fc675460a089ba6414fe4693b83ea3fdd8e938832a6&o='
+                alt=''
+            />
+            <span className='fpName'>Aparthotel Stare Miasto</span>
+            <span className='fpCity'>London</span>
+            <span className='fpPrice'>Starting from $120</span>
+            <div className='fpRating'>
+                <button>8.9</button>
+                <span>Excellent</span>
+            </div>
+        </div>
+        <div className='fpItem'>
+            <img
+                src='https://cf.bstatic.com/xdata/images/xphoto/square300/57584488.webp?k=bf724e4e9b9b75480bbe7fc675460a089ba6414fe4693b83ea3fdd8e938832a6&o='
+                alt=''
+            />
+            <span className='fpName'>Aparthotel Stare Miasto</span>
+            <span className='fpCity'>London</span>
+            <span className='fpPrice'>Starting from $120</span>
+            <div className='fpRating'>
+                <button>8.9</button>
+                <span>Excellent</span>
+            </div>
+        </div>
+        <div className='fpItem'>
+            <img
+                src='https://cf.bstatic.com/xdata/images/xphoto/square300/57584488.webp?k=bf724e4e9b9b75480bbe7fc675460a089ba6414fe4693b83ea3fdd8e938832a6&o='
+                alt=''
+            />
+            <span className='fpName'>Aparthotel Stare Miasto</span>
+            <span className='fpCity'>London</span>
+            <span className='fpPrice'>Starting from $120</span>
+            <div className='fpRating'>
+                <button>8.9</button>
+                <span>Excellent</span>
+            </div>
+        </div>
+        <div className='fpItem'>
+            <img
+                src='https://cf.bstatic.com/xdata/images/xphoto/square300/57584488.webp?k=bf724e4e9b9b75480bbe7fc675460a089ba6414fe4693b83ea3fdd8e938832a6&o='
+                alt=''
+            />
+            <span className='fpName'>Aparthotel Stare Miasto</span>
+            <span className='fpCity'>London</span>
+            <span className='fpPrice'>Starting from $120</span>
+            <div className='fpRating'>
+                <button>8.9</button>
+                <span>Excellent</span>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default FeaturedProperties
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,9 @@ import './home.css';
 import Navbar from '../../components/navbar/Navbar';
 import Header from '../../components/header/Header';
 import Featured from '../../components/featured/Featured';
+import PropertyList from '../../components/propertyList/PropertyList';
+import FeaturedProperties from '../../components/featured/FeaturedProperties';
+import MailList from '../../components/mailList/MailList';
 
 export const Home = () => {
   return (
@@ -11,6 +14,11 @@ export const Home = () => {
         <Header/>
         <div className='homeContainer'>
             <Featured/>
+            <h1 className='homeTitle'>Browse by property type</h1>
+            <PropertyList/>
+            <h1 className='homeTitle'>Homes guests love</h1>
+            <FeaturedProperties/>
+            <MailList/>
         </div>
     </div>
   )
