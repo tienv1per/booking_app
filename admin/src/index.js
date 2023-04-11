643acd56b6a3c84a46a3f778eb1c3aacf7f4e0b9
tienv1per <tienv1perds@gmail.com>
2023-04-11 21:47:30 +0700

    add admin page

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { DarkModeContextProvider } from "./context/darkModeContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <App />
+ReactDOM.render(
+  <React.StrictMode>
+    <DarkModeContextProvider>
+      <App />
+    </DarkModeContextProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-
